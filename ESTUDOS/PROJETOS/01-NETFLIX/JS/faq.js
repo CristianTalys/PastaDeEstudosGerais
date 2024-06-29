c8a33e9362a192a527d9b6551d180f01e640c646
Cristian Talys Braga Borges <contato.jinkai@gmail.com>
2024-06-29 19:25:38 -0300

    Quase finalizado projeto Netflix

---
+++ ESTUDOS/PROJETOS/01-NETFLIX/JS/faq.js
@@ -0,0 +1,29 @@
+function mostraDiv1() {
+    let resposta = document.querySelector('#r1');
+
+    resposta = resposta.className == 'mostraResposta' ? resposta.className = 'resposta1' : resposta.className = 'mostraResposta';
+}
+
+function mostraDiv2() {
+    let resposta = document.querySelector('#r2');
+
+    resposta = resposta.className == 'mostraResposta' ? resposta.className = 'resposta2' : resposta.className = 'mostraResposta';
+}
+
+function mostraDiv3() {
+    let resposta = document.querySelector('#r3');
+
+    resposta = resposta.className == 'mostraResposta' ? resposta.className = 'resposta3' : resposta.className = 'mostraResposta';
+}
+
+function mostraDiv4() {
+    let resposta = document.querySelector('#r4');
+
+    resposta = resposta.className == 'mostraResposta' ? resposta.className = 'resposta4' : resposta.className = 'mostraResposta';
+}
+
+function mostraDiv5() {
+    let resposta = document.querySelector('#r5');
+
+    resposta = resposta.className == 'mostraResposta' ? resposta.className = 'resposta5' : resposta.className = 'mostraResposta';
+}
---
+++ ESTUDOS/PROJETOS/01-NETFLIX/JS/signature.js
@@ -0,0 +1,11 @@
+let mySignature = document.getElementById('showMe');
+
+
+
+function showSignature() {
+    mySignature.style.top = '0px';
+};
+
+function hideSignature() {
+    mySignature.style.top = '-1300px';
+};
