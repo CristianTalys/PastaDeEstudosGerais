1de8b312584b281205207eab7d827d9d64efe152
Cristian Talys Braga Borges <contato.jinkai@gmail.com>
2024-07-02 19:08:50 -0300

    Atualizado

--- ESTUDOS/PROJETOS/03-FINANÇAS-PESSOAIS/JS/dark-light.js
+++ ESTUDOS/PROJETOS/03-FINANÇAS-PESSOAIS/JS/dark-light.js
@@ -1,13 +1,18 @@
 let btnTema = document.getElementById('btnTema');
 let fundo = document.getElementById('fundo');
+let main = document.getElementById('main');
 
 
 function trocaTema() {
   if (btnTema.className == 'btnDarkTheme') {
     btnTema.className = 'btnLightTheme';
     fundo.className = 'darkTheme';
+    main.style.color = 'white';
   }else if (btnTema.className == 'btnLightTheme') {
     btnTema.className = 'btnDarkTheme';
     fundo.className = 'lightTheme';
+    main.style.color = 'black';
+
   };
-}
+};
+
